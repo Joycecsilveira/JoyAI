eb1a3bea19ed3405efbabc04a2912e9d06dfbd9c
Joyce Silveira <joycesilveiragp@gmail.com>
2026-04-03 02:22:47 -0300

    feat: integração inicial com IA (ollama) e chat funcional

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import flet as ft
+import requests
+
+
+def toask_ollama(prompt):
+    response = requests.post(
+        "http://localhost:11434/api/generate",
+        json={"model": "llama3", "prompt": prompt, "stream": False},
+    )
+    return response.json()["response"]
 
 
 def create_chat_message(text, user_name):
@@ -7,27 +16,32 @@ def create_chat_message(text, user_name):
 
 def main(page: ft.Page):
     page.title = "JoyAI - Chatbox"
-    chat_history = ft.Column(scroll=ft.ScrollMode.AUTO)
     page.theme_mode = ft.ThemeMode.DARK
 
-    user_input = ft.TextField(
-        label="Olá, eu sou a   JoyAI. Por onde vamos começar hoje?"
-    )
+    chat_history = ft.Column(scroll=ft.ScrollMode.AUTO, expand=True)
+    user_input = ft.TextField(label="Digite algo", expand=True)
 
     def send_message(e):
-        if user_input.value != "":
-            new_message = create_chat_message(user_input.value, "você")
-            chat_history.controls.append(new_message)
+        if user_input.value:
 
-        user_input.value = ""
-        user_input.focus()
-        page.update()
+            user_text = user_input.value
 
-    send_button = ft.ElevatedButton("Enviar", icon=ft.Icons.SEND, on_click=send_message)
+            chat_history.controls.append(create_chat_message(user_text, "Você"))
+
+            user_input.value = ""
+            page.update()
+
+            response = toask_ollama(user_text)
+
+            chat_history.controls.append(create_chat_message(response, "JoyIA"))
+
+            page.update()
+            user_input.focus()
+
+    send_button = ft.IconButton(icon=ft.Icons.SEND, on_click=send_message)
     input_row = ft.Row([user_input, send_button])
 
     page.add(chat_history, input_row)
 
 
-# Comando que inicia o app
 ft.app(target=main)
