48b1e52543c4dd4cd73e1ea92409ed8b43554404
Arthur Prasniski <prasniski@outlook.com>
2026-04-02 23:48:15 -0300

    commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main (page: ft.Page):
     page.title = "Meu Primeiro App IA - Joyce"
     page.theme_mode = ft.ThemeMode.DARK
  
-    # Criando um elemento de texto (O famoso Olá Mundo)
+    # Criando um elemento de texto (O famoso Olá Mundo!)
     user_input = ft.TextField(label= "Olá, eu sou a   JoyAI. Por onde vamos começar hoje?")
     ai_response = ft.Text(value="Aguardando...")
 
